5df1448ecca7f7b4ded6ffa55a2133a76b1be014
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-10-18 16:36:17 -0600

    updating repo

--- Inspector.java
+++ Inspector.java
@@ -10,16 +10,10 @@ class Inspector {
         // Get the name of the immediate superclass
         
         printSuperClass(obj);
-
-
-
-
+        
 
         // Call helper method to print all interfaces used by a class
         printInterfaces(obj);
-
-
-
 
         // Helper method to print all methods the class decarles as well as...
         /* 
@@ -31,9 +25,7 @@ class Inspector {
 
          printMethodsAndMethodInfo(obj);
 
-
-
-        /*
+         /*
             Helper method to print out constructor info a class declares as well as...
             
                 1. the parameter types
@@ -42,10 +34,7 @@ class Inspector {
 
           printConstructorAndInfo(obj);
 
-
-
-
-            /*
+          /*
               Helper method which prints out all fields as well as...
 
                 1. their type
@@ -53,13 +42,26 @@ class Inspector {
 
            */
 
-           printFields(obj);
+          printFields(obj);
 
 
-        Class<?>objClass = obj.getClass();
 
+
+
+          /*
+            Helper method which gets the current value of each field, if the field is a ref
+            to another object AND the recursive flag is set to FALSE, just print the reference value
+          */
+
+          
+
+    }
+
+
+    public void printFieldValues(Object obj, boolean recursive) {
         System.out.println("\n");
         System.out.println("---- PRINTING FIELD VALUES ---- \n");
+        Class<?> objClass = obj.getClass();
         Field[] fields = objClass.getDeclaredFields();
         for(Field f : fields) {
             f.setAccessible(true);
@@ -84,14 +86,24 @@ class Inspector {
             }
         }
 
+    }
 
+
+
+    public void printFields(Object obj) {
+        Class<?> objClass = obj.getClass();
         for(Field f : objClass.getDeclaredFields()) {
             f.setAccessible(true);
             System.out.println("Field Name: " + f.getName());
             System.out.println("Field Type: " + f.getType().getName());
             System.out.println("Field Modifier: " + Modifier.toString(f.getModifiers()));
         }
+    }
+    
 
+
+    public void printConstructorAndInfo(Object obj) {
+        Class<?> objClass = obj.getClass();
         System.out.println("---- PRINT CONSTRUCTOR AND CONSTRUCTOR INFORMATION ---- \n \n");
 
         for(Constructor<?> constructor : objClass.getDeclaredConstructors()) {
@@ -101,49 +113,6 @@ class Inspector {
         }
         System.out.println("\n \n");
         System.out.println("---- END OF PRINTING CONSTRUCTOR AND CONSTRUCTOR INFORMATION ----");
-
-
-
-
-        System.out.println("---- PRINT INTERFACES ---- \n \n");
-        Class[] interfaces = objClass.getInterfaces();
-
-        for(int i = 0; i < interfaces.length; i++) {
-            String interfaceName = interfaces[i].getName();
-            System.out.println("Interface Name: " + interfaceName);
-        }
-        System.out.println("\n \n");
-        System.out.println("---- END OF PRINTING ALL INTERFACES ----");
-
-    }
-
-    public void printSuperClass(Object obj) {
-        System.out.println("---- PRINTING SUPER CLASS ---- \n \n");
-        Class<?> superClass = obj.getClass().getSuperclass();
-        if(superClass != null) {
-            System.out.println("Superclass Name:" + superClass.getName());
-        }
-        else {
-            System.out.println("No Superclass Found.");
-        }
-        System.out.println("\n \n");
-        System.out.println("---- END OF PRINTING SUPERCLASS ----");
-    }
-
-
-
-    // helper method that prints out all interfaces of a given object
-    public void printInterfaces(Object obj) {
-        Class<?> objClass = obj.getClass();
-        System.out.println("---- PRINT INTERFACES ---- \n \n");
-        Class[] interfaces = objClass.getInterfaces();
-
-        for(int i = 0; i < interfaces.length; i++) {
-            String interfaceName = interfaces[i].getName();
-            System.out.println("Interface Name: " + interfaceName);
-        }
-        System.out.println("\n \n");
-        System.out.println("---- END OF PRINTING ALL INTERFACES ----");
     }
 
 
@@ -162,30 +131,31 @@ class Inspector {
 
     }
 
-    public void printConstructorAndInfo(Object obj) {
-        Class<?> objClass = obj.getClass();
-        System.out.println("---- PRINT CONSTRUCTOR AND CONSTRUCTOR INFORMATION ---- \n \n");
 
-        for(Constructor<?> constructor : objClass.getDeclaredConstructors()) {
-            System.out.println("Constructor Name: " + constructor.getName());
-            System.out.println("Modifiers: " + Modifier.toString(constructor.getModifiers()));
-            System.out.println("Parameter Types: " + Arrays.toString(constructor.getParameterTypes()));
+    public void printSuperClass(Object obj) {
+        System.out.println("---- PRINTING SUPER CLASS ---- \n \n");
+        Class<?> superClass = obj.getClass().getSuperclass();
+        if(superClass != null) {
+            System.out.println("Superclass Name:" + superClass.getName());
+        }
+        else {
+            System.out.println("No Superclass Found.");
         }
         System.out.println("\n \n");
-        System.out.println("---- END OF PRINTING CONSTRUCTOR AND CONSTRUCTOR INFORMATION ----");
+        System.out.println("---- END OF PRINTING SUPERCLASS ----");
     }
 
-
-    public void printFields(Object obj) {
+    // helper method that prints out all interfaces of a given object
+    public void printInterfaces(Object obj) {
         Class<?> objClass = obj.getClass();
-        for(Field f : objClass.getDeclaredFields()) {
-            f.setAccessible(true);
-            System.out.println("Field Name: " + f.getName());
-            System.out.println("Field Type: " + f.getType().getName());
-            System.out.println("Field Modifier: " + Modifier.toString(f.getModifiers()));
+        System.out.println("---- PRINT INTERFACES ---- \n \n");
+        Class[] interfaces = objClass.getInterfaces();
+
+        for(int i = 0; i < interfaces.length; i++) {
+            String interfaceName = interfaces[i].getName();
+            System.out.println("Interface Name: " + interfaceName);
         }
+        System.out.println("\n \n");
+        System.out.println("---- END OF PRINTING ALL INTERFACES ----");
     }
-
-
-
 }
